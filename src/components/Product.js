8e4a8b6abdaaa28230bd4dce385d7b7580478ba4
Ravi Vadukul <ravivadukul@ipangram.com>
2022-09-21 20:27:31 +0530

    (Thunks,API Call Reduser)

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,23 +1,37 @@
 import React, { useEffect, useState } from 'react'
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { add } from '../store/cartslice';
+import { fetchProducts } from '../store/productSlice';
+import { STATUSES } from '../store/productSlice';
 
 export default function Product() {
-    const [products, setproducts] = useState([]);
+    // const [products, setproducts] = useState([]);
+    const { data: products, status } = useSelector((state) => state.product);
     const dispatch = useDispatch();
+
     useEffect(() => {
-        const fetchproducts = async () => {
-            const res = await fetch('https://fakestoreapi.com/products');
-            const data = await res.json();
-            console.log(data);
-            setproducts(data)
-        }
-        fetchproducts()
+        dispatch(fetchProducts())
+        // const fetchproducts = async () => {
+        //     const res = await fetch('https://fakestoreapi.com/products');
+        //     const data = await res.json();
+        //     console.log(data);
+        //     setproducts(data)
+        // }
+        // fetchproducts()
     }, [])
 
     const addproduct = (product) => {
         dispatch(add(product))
     }
+
+    if(status == STATUSES.LOADING) {
+      return  <h1> Loading </h1>
+    }
+
+    if(status == STATUSES.ERROR) {
+        return  <h1> SOMENTHING WANT WRONG </h1>
+      }
+
   return (
     <div className='productsWrapper'>{
         products.map(product => (
--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,12 @@
 // Hii
 import { configureStore } from "@reduxjs/toolkit";
 import cartReducer from './cartslice'
+import productReducer from './productSlice'
+
 const store = configureStore({
     reducer: {
-        cart : cartReducer
+        cart : cartReducer,
+        product : productReducer
     }
 })
 export default store;
